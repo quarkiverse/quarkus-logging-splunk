0929af7f7830694739780201c0cecb535a195e5e
jcjveraa <3942301+jcjveraa@users.noreply.github.com>
2025-03-07 15:13:59 +0100

    restore copyright block in HttpEventCollectorResendMiddlewareTest

--- runtime/src/test/java/io/quarkiverse/logging/splunk/middleware/HttpEventCollectorResendMiddlewareTest.java
+++ runtime/src/test/java/io/quarkiverse/logging/splunk/middleware/HttpEventCollectorResendMiddlewareTest.java
@@ -1,4 +1,23 @@
 package io.quarkiverse.logging.splunk.middleware;
+/**
+ * @copyright
+ *
+ * Copyright 2013-2015 Splunk, Inc.
+ * Derived from https://github.com/splunk/splunk-library-javalogging/pull/287
+ * by Simon Hege. Modifications include updating the unit testing framework to JUnit 5.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"): you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -6,9 +25,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import com.splunk.logging.HttpEventCollectorMiddleware;
 import org.junit.jupiter.api.Test;
 
-import com.splunk.logging.HttpEventCollectorMiddleware;
 
 public class HttpEventCollectorResendMiddlewareTest {
 
